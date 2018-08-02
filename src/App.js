2ae8dd15408f754bbf198ccc435b227035b0b66c
xieyankun <kiroader@gmail.com>
2018-08-02 21:55:46 +0800

    feat: add codemirror

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CodeMirror from 'codemirror';
+import './common/js/codemirror/lib/codemirror.css'
+// import './common/js/codemirror/mode/javascript/javascript'
 
 class App extends Component {
+
+  componentDidMount() {
+    const codemirror = document.querySelector('.codemirror');
+    console.log('CodeMirror', codemirror);
+    const myCodeMirror = CodeMirror(codemirror, {
+      value: "function myScript(){return 100;}\n",
+      mode:  "javascript"
+    });
+  }
+
   render() {
+    console.log(CodeMirror);
     return (
       <div className="App">
         <header className="App-header">
@@ -13,6 +27,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="codemirror">
+          
+        </div>
       </div>
     );
   }
